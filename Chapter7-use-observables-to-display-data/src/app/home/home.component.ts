cbdb4679e401e25112435529c73c2a2ca1f0740f
lu_chan <lou.97213@gmail.com>
2025-02-23 13:15:17 +0000

    Chapter7-use-rxjs-observables-to-display-data

---
+++ Chapter7-use-observables-to-display-data/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { HighlightDirective } from '../highlight.directive';
+import { MovieItemComponent } from '../movie-item/movie-item.component';
+import { Movie } from '../model/movie.model';
+import { MoviesService } from '../services/movies.service';
+import { FavoritesService } from '../services/favorites.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HighlightDirective,
+    MovieItemComponent,
+    CommonModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  protected movies: Observable<Array<Movie>> = inject(MoviesService).getMovies();
+  protected favoritesService = inject(FavoritesService);
+}
